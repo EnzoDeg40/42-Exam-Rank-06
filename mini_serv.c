0fb2992d686a670374a53ec69e42428205c03629
josephcheel <josephcheelnakashima@gmail.com>
2024-07-19 19:58:49 +0200

    passed exam

--- mini_serv.c
+++ mini_serv.c
@@ -26,8 +26,8 @@ fd_set read_set;
 fd_set write_set;
 fd_set current;
 
-char	send_buffer[MAX_MSG_SIZE + 20];
-char	recv_buffer[MAX_MSG_SIZE + 20];
+char	send_buffer[MAX_MSG_SIZE];
+char	recv_buffer[MAX_MSG_SIZE];
 
 void putstr(int fd, char *str)
 {
@@ -61,80 +61,85 @@ void	send_broadcast(int accepted)
 int main(int ac, char **av)
 {
 	if (ac != 2)
-        	err("Wrong number of arguments");
+		err("Wrong number of arguments\n");
+	int sockfd, connfd;
+	struct sockaddr_in servaddr; 
 
-	bzero(clients, sizeof(clients));
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1)
+		err(NULL);
+	bzero(&servaddr, sizeof(servaddr)); 
+
 	FD_ZERO(&current);
+	FD_SET(sockfd, &current);
+	maxfd = sockfd;
 	
-	int serverfd;
-	if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	err(NULL);
-	setsockopt(serverfd, SO_REUSEPORT, 0, 0 , 0);
-	
-	maxfd = serverfd;
-	FD_SET(serverfd, &current);
-	
-	struct sockaddr_in  serveraddr;
-	bzero(&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons(atoi(av[1]));
-	
-	if (bind(serverfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
-	 err(NULL);
-	if (listen(serverfd, BACK_LOG) == -1)
-	err(NULL);
-	
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(atoi(av[1])); 
+  
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+		err(NULL);
+
+	if (listen(sockfd, 10) != 0)
+		err(NULL);
 	while (1)
 	{
 		read_set = write_set = current;
 		if (select(maxfd + 1, &read_set, &write_set, NULL, NULL) == -1)
-				continue;
-		
+			err(NULL);
 		for (int fd = 0; fd <= maxfd; fd++)
 		{
-		    if (FD_ISSET(fd, &read_set))
-		    {
-			if (fd == serverfd)
+			if (FD_ISSET(fd, &read_set))
 			{
-			    socklen_t   len;
-			    int clientfd = accept(serverfd, (struct sockaddr *)&serveraddr, &len);
-			    if (clientfd == -1)
-							continue;
-			    if (clientfd > maxfd)
-							maxfd = clientfd;
-			    clients[clientfd].id = curr_id++;
-			    FD_SET(clientfd, &current);
-			    sprintf(send_buffer, "server: client %d just arrived\n", clients[clientfd].id);
-			    broadcast_send(clientfd);
-			}
-			else
-			{
-			    int ret; 
-			    if ((ret = recv(fd, recv_buffer, sizeof(recv_buffer), 0)) <= 0)
-			    {
-				sprintf(send_buffer, "server: client %d just left\n", clients[fd].id);
-				broadcast_send(fd);
-				FD_CLR(fd, &current);
-				close(fd);
-			    }
-			    else
-			    {
-			      for (int i = 0, j = strlen(clients[fd].msg); i < ret; i++, j++)
-			      {
-				    clients[fd].msg[j] = recv_buffer[i];
-				    if (clients[fd].msg[j] == '\n')
-				    {
-					clients[fd].msg[j] = '\0';
-					sprintf(send_buffer, "client %d: %s\n", clients[fd].id, clients[fd].msg);
-					broadcast_send(fd);
-					bzero(clients[fd].msg, sizeof(clients[fd].msg));
-					j = -1;
-				    }
+				if (fd == sockfd)
+				{
+					struct sockaddr_in cli;
+					bzero(&cli, sizeof(cli));
+					socklen_t len = sizeof(cli);
+					connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+					if (connfd < 0)
+						err(NULL);
+					if (connfd > maxfd)
+						maxfd = connfd;
+					clients[connfd].id = current_id;
+					current_id++;
+					FD_SET(connfd, &current);
+					sprintf(send_buffer, "server: client %d just arrived\n", clients[connfd].id);
+					send_broadcast(connfd);
 				}
-			    }
+				else
+				{
+					int ret = recv(fd, recv_buffer, MAX_MSG_SIZE, 0);
+					if (ret <= 0)
+					{
+						sprintf(send_buffer, "server: client %d just left\n", clients[fd].id);
+						send_broadcast(fd);
+						FD_CLR(fd, &current);
+						close(fd);
+					}
+					else
+					{
+						for (int i = 0, j = strlen(clients[fd].msg); i < ret; i++, j++)
+						{
+							clients[fd].msg[j] = recv_buffer[i];
+							if (clients[fd].msg[j] == '\n')
+							{
+								clients[fd].msg[j] = '\0';
+								sprintf(send_buffer, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+								send_broadcast(fd);
+								bzero(clients[fd].msg, strlen(clients[fd].msg));
+								j = -1;
+							}
+						}
+					}
+				}
 			}
-		    }
+		
 		}
 	}
+	
 }
